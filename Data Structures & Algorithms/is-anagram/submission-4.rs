95d97f45893c3048e83edf7dd1c73849f82d7379
alem-147 <81839228+alem-147@users.noreply.github.com>
2026-08-04 17:51:48 -0500

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.rs
@@ -0,0 +1,21 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn is_anagram(s: String, t: String) -> bool {
+        let mut s_occs: HashMap<char, i32> = HashMap::new();
+        let mut t_occs: HashMap<char, i32> = HashMap::new();
+        
+        // for (s_let, t_let) in s.chars().zip(t.chars()) {
+        //     *s_occs.entry(s_let).or_insert(0) += 1;
+        //     *t_occs.entry(t_let).or_insert(0) += 1;
+        // }
+
+        for t_let in t.chars() {
+            *t_occs.entry(t_let).or_insert(0) += 1;
+        }
+        for s_let in s.chars() {
+            *s_occs.entry(s_let).or_insert(0) += 1;
+        }
+        s_occs == t_occs
+    }
+}
