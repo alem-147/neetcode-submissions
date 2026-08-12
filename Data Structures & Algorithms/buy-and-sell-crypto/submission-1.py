e8a91c0000a9aff5602b4d9b61faf768d5da8227
alem-147 <81839228+alem-147@users.noreply.github.com>
2026-08-12 08:25:07 -0500

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.py
@@ -0,0 +1,27 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        """
+        precompute the minimum price to that point
+        and max price after that point
+        the greatest difference is the best value
+
+        how to figure out which index
+        """
+        n = len(prices)
+        suffix_max = [0] * n 
+        prefix_min = [0] * n
+
+
+        prefix_min[0] = prices[0]
+        for i in range(1, n):
+            prefix_min[i] = min(prefix_min[i-1], prices[i])
+        
+        suffix_max[-1] = prices[-1]
+        for i in reversed(range(0, n-1)):
+            suffix_max[i] = max(suffix_max[i+1], prices[i])
+
+        diff = [] 
+        for pre, suf in zip(prefix_min, suffix_max):
+            diff.append(suf - pre)
+        
+        return max(0, max(diff))
